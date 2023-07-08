a4ac5d26e70cea872c44d76831339d295a842fd4
adah <joshuaadah6@gmail.com>
2023-07-08 16:56:53 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import TodoList from './features/todo/TodoList'
-import Layout from './components/Layout'
 
 const App = () => {
   return (
